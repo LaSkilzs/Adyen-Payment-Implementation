e4842df06473eebe9fe7935e4406d5da3284ec27
LaSkilzs <lfountain123@gmail.com>
2021-06-19 16:33:29 -0400

    Updated data and page format for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from  './components/navbar'
 import Product from  './components/product';
+import ProductDetail from './components/product/ProductDetail';
 import Cart from  './components/cart';
 
 
@@ -10,6 +11,7 @@ const App = () => {
      {/* <h1>Hello World</h1> */}
      <Navbar/>
      <Product/>
+     <ProductDetail />
      <Cart />
     </div>
   );
