91ba9835e182ad9618becd91d04dd2c2dae19f7a
LaSkilzs <lfountain123@gmail.com>
2021-06-22 17:20:50 -0400

    added path module to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const dotenv = require("dotenv");
+const path = require('path');
 
 
 const app = express();
