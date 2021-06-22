b25955ec0de686942b7669e8972270a2288323bd
LaSkilzs <lfountain123@gmail.com>
2021-06-22 18:49:36 -0400

    fixing cors

--- client/src/utility/API.js
+++ client/src/utility/API.js
@@ -3,7 +3,7 @@ class API {
     static async callServer(url, data) {
       const res = await fetch(url, {
         method: "POST",
-        // mode: 'no-cors',
+        mode: 'no-cors',
         body: data ? JSON.stringify(data) : "",
         headers: {
           "Content-Type": "application/json",
