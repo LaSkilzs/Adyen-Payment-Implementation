c08128b92a2491970e43b79bc891ce713cc644f8
LaSkilzs <lfountain123@gmail.com>
2021-06-23 18:12:19 -0400

    removed cors headers

--- server.js
+++ server.js
@@ -27,12 +27,12 @@ var corsOptions = {
  console.log('cors options', corsOptions);
 
 //  CORS Headers
- app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", domainOrigin); // update to match the domain you will make the request from
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   res.header('Access-Control-Allow-Credentials', 'true')
-   next();
-  });
+//  app.use(function(req, res, next) {
+//    res.header("Access-Control-Allow-Origin", domainOrigin); // update to match the domain you will make the request from
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    res.header('Access-Control-Allow-Credentials', 'true')
+//    next();
+//   });
   
 app.use(cors(corsOptions));
 
