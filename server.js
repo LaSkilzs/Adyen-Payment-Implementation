2e59856af7c4704ba34898c758ea7f4ad8da475c
LaSkilzs <lfountain123@gmail.com>
2021-06-23 19:10:35 -0400

    updated parse json file

--- server.js
+++ server.js
@@ -69,8 +69,7 @@ app.post("/api/getPaymentMethods", async (req, res) => {
       channel: "Web",
       merchantAccount: config.merchantAccount,
     });
-    // res.json(response);
-    res.JSON.parse(response);
+    res.json(response);
     console.log('response', res.json(response))
   } catch (err) {
     console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
