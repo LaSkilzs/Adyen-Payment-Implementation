4738f149bba38e9ebeeddee79867adbc7616fa66
LaSkilzs <lfountain123@gmail.com>
2021-06-23 15:07:19 -0400

    Updated Server.js for development

--- server.js
+++ server.js
@@ -12,24 +12,16 @@ var whitelist = ['http://localhost:3000', 'https://adyen-api-implementation.hero
 
 console.log('environment', process.env.NODE_ENV === 'production');
 
-// const domainOrigin = process.env.NODE_ENV === 'production' ? whitelist[1] : whitelist[0]
-const domainOrigin = 'https://adyen-api-implementation.herokuapp.com'
+const domainOrigin = process.env.NODE_ENV === 'production' ? whitelist[1] : whitelist[0]
+// const domainOrigin = 'https://adyen-api-implementation.herokuapp.com'
 
 var corsOptions = {
-  origin: domainOrigin
+  origin: domainOrigin |
 }
  console.log('cors options', corsOptions);
 
 app.use(cors(corsOptions));
 
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'https://adyen-api-implementation.herokuapp.com'); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-  next();
-});
 
 // parsing the .env file and assigning it to process.env
 dotenv.config({
@@ -61,6 +53,14 @@ const originStore = {};
 
 /* ################# API ENDPOINTS ###################### */
 app.get("/api/getPaymentDataStore", async (req, res) => res.json(paymentStore));
+
+// CORS Headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", domainOrigin); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Credentials', 'true')
+  next();
+});
 
 // Get payment methods
 app.post("/api/getPaymentMethods", async (req, res) => {
